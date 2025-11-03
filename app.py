d048c402c88799ce889ba229ba4bc028c3b68416
Samridh Ramesha <samridhramesha@gmail.com>
2025-11-03 23:24:29 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+# If HTML files are in same directory as this file
+app = Flask(__name__, template_folder='.')
+
+# MySQL environment variables (from docker-compose)
+MYSQL_USER = os.environ.get('MYSQL_USER', 'flaskuser')
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'flaskpass')
+MYSQL_HOST = os.environ.get('MYSQL_HOST', 'db')  # Service name in docker-compose
+MYSQL_DB = os.environ.get('MYSQL_DB', 'flaskdb')
+
+# Configure MySQL connection
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+# ROUTES
+@app.route('/')
+def index():
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        new_user = User(name=name, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_user(id):
+    user = User.query.get_or_404(id)
+    if request.method == 'POST':
+        user.name = request.form['name']
+        user.email = request.form['email']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', user=user)
+
+@app.route('/delete/<int:id>')
+def delete_user(id):
+    user = User.query.get_or_404(id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
